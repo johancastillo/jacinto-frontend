f68ca30390a1e9e1b52959ef8380ee8be90e316e
Johan Castillo <jcjohan2707@gmail.com>
2021-10-12 09:41:50 -0400

    Request

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,18 @@
-
-
+import {useEffect} from "react"
+import axios from "axios"
 
 
 const Home = () => {
+
+    useEffect(() => {
+        axios.get("http://ec2-54-147-59-92.compute-1.amazonaws.com/api/items")
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => console.log(error));
+
+    }, [])
+
     return(
         <div>
             <h1>All fine!</h1>
